5d226616257808b398617ad832d493904cd4b43c
bienvenue cedrick <cedrick4smith@gmail.com>
2026-06-26 16:59:17 +0200

    Add isEmailVerified to AuthAuthenticated Equatable props

--- lib/features/auth/presentation/cubit/auth_state.dart
+++ lib/features/auth/presentation/cubit/auth_state.dart
@@ -16,7 +16,7 @@ class AuthAuthenticated extends AuthState {
   const AuthAuthenticated(this.user);
 
   @override
-  List<Object?> get props => [user.uid, user.isOnboarded];
+  List<Object?> get props => [user.uid, user.isOnboarded, user.isEmailVerified];
 }
 
 class AuthUnauthenticated extends AuthState {}
