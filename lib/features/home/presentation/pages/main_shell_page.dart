0fed5c6e6eabd8d88a789d9fad348ee7073fdd5f
bienvenue cedrick <cedrick4smith@gmail.com>
2026-07-03 12:20:47 +0200

    Make MainShellPage role-aware: startup gets Home/Applicants/Profile, student gets Home/Explore/Applications/Profile

--- lib/features/home/presentation/pages/main_shell_page.dart
+++ lib/features/home/presentation/pages/main_shell_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../applications/presentation/pages/applications_tab_page.dart';
+import '../../../applications/presentation/pages/startup_applicants_tab_page.dart';
+import '../../../auth/data/models/user_model.dart';
+import '../../../auth/presentation/cubit/auth_cubit.dart';
 import '../../../opportunities/presentation/pages/explore_tab_page.dart';
 import '../../../profile/presentation/pages/profile_tab_page.dart';
 import 'home_tab_page.dart';
@@ -13,15 +17,30 @@ class MainShellPage extends StatefulWidget {
 
 class _MainShellPageState extends State<MainShellPage> {
   int _currentIndex = 0;
+  late final bool _isStartup;
+  late final List<Widget> _pages;
 
   void _goToExplore() => setState(() => _currentIndex = 1);
 
-  late final List<Widget> _pages = [
-    HomeTabPage(onExploreTap: _goToExplore),
-    const ExploreTabPage(),
-    const ApplicationsTabPage(),
-    const ProfileTabPage(),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    final auth = context.read<AuthCubit>().state;
+    _isStartup = auth is AuthAuthenticated && auth.user.role == UserRole.startup;
+
+    _pages = _isStartup
+        ? [
+            HomeTabPage(onExploreTap: _goToExplore),
+            const StartupApplicantsTabPage(),
+            const ProfileTabPage(),
+          ]
+        : [
+            HomeTabPage(onExploreTap: _goToExplore),
+            const ExploreTabPage(),
+            const ApplicationsTabPage(),
+            const ProfileTabPage(),
+          ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +49,25 @@ class _MainShellPageState extends State<MainShellPage> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (i) => setState(() => _currentIndex = i),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.explore_rounded), label: 'Explore'),
-          BottomNavigationBarItem(icon: Icon(Icons.assignment_rounded), label: 'Applications'),
-          BottomNavigationBarItem(icon: Icon(Icons.person_rounded), label: 'Profile'),
-        ],
+        items: _isStartup
+            ? const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_rounded), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.people_alt_rounded), label: 'Applicants'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person_rounded), label: 'Profile'),
+              ]
+            : const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_rounded), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.explore_rounded), label: 'Explore'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.assignment_rounded), label: 'Applications'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person_rounded), label: 'Profile'),
+              ],
       ),
     );
   }
