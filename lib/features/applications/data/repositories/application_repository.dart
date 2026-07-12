d922db62f03c7704a541d5d2b78e3c30279525e3
bienvenue cedrick <cedrick4smith@gmail.com>
2026-07-12 22:48:20 +0200

    Remove re-fetch in submit, add uploadResume to Cloudinary

--- lib/features/applications/data/repositories/application_repository.dart
+++ lib/features/applications/data/repositories/application_repository.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import '../models/application_model.dart';
 
 class ApplicationRepository {
@@ -37,10 +41,23 @@ class ApplicationRepository {
     );
   }
 
-  Future<ApplicationModel> submit(ApplicationModel application) async {
-    final ref = await _col.add(application.toFirestore());
-    final doc = await ref.get();
-    return ApplicationModel.fromFirestore(doc);
+  Future<void> submit(ApplicationModel application) => _col.add(application.toFirestore());
+
+  Future<String> uploadResume(String applicantId, String opportunityId, Uint8List bytes) async {
+    final cloudName = dotenv.env['CLOUDINARY_CLOUD_NAME']!;
+    final preset = dotenv.env['CLOUDINARY_UPLOAD_PRESET']!;
+    final uri = Uri.parse('https://api.cloudinary.com/v1_1/$cloudName/auto/upload');
+    final publicId = 'resumes/${applicantId}_$opportunityId';
+    final request = http.MultipartRequest('POST', uri)
+      ..fields['upload_preset'] = preset
+      ..fields['public_id'] = publicId
+      ..files.add(http.MultipartFile.fromBytes('file', bytes, filename: 'resume.pdf'));
+    final streamed = await request.send();
+    final body = jsonDecode(await streamed.stream.bytesToString());
+    if (streamed.statusCode != 200) {
+      throw Exception('Upload failed: ${body['error']?['message'] ?? 'Unknown error'}');
+    }
+    return body['secure_url'] as String;
   }
 
   Future<void> updateStatus(String id, ApplicationStatus status) => _col.doc(id).update({
