752ae5a94875c3ae21008f554dc44dde6e18cb65
bienvenue cedrick <cedrick4smith@gmail.com>
2026-07-13 12:35:12 +0200

    Switch resume upload from Cloudinary to Firebase Storage

--- lib/features/applications/data/repositories/application_repository.dart
+++ lib/features/applications/data/repositories/application_repository.dart
@@ -1,12 +1,11 @@
-import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import '../models/application_model.dart';
 
 class ApplicationRepository {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   CollectionReference get _col => _db.collection('applications');
 
@@ -44,20 +43,9 @@ class ApplicationRepository {
   Future<void> submit(ApplicationModel application) => _col.add(application.toFirestore());
 
   Future<String> uploadResume(String applicantId, String opportunityId, Uint8List bytes) async {
-    final cloudName = dotenv.env['CLOUDINARY_CLOUD_NAME']!;
-    final preset = dotenv.env['CLOUDINARY_RESUME_PRESET']!;
-    final uri = Uri.parse('https://api.cloudinary.com/v1_1/$cloudName/raw/upload');
-    final publicId = 'resumes/${applicantId}_$opportunityId';
-    final request = http.MultipartRequest('POST', uri)
-      ..fields['upload_preset'] = preset
-      ..fields['public_id'] = publicId
-      ..files.add(http.MultipartFile.fromBytes('file', bytes, filename: 'resume.pdf'));
-    final streamed = await request.send();
-    final body = jsonDecode(await streamed.stream.bytesToString());
-    if (streamed.statusCode != 200) {
-      throw Exception('Upload failed: ${body['error']?['message'] ?? 'Unknown error'}');
-    }
-    return body['secure_url'] as String;
+    final ref = _storage.ref('resumes/${applicantId}_$opportunityId.pdf');
+    await ref.putData(bytes, SettableMetadata(contentType: 'application/pdf'));
+    return await ref.getDownloadURL();
   }
 
   Future<void> updateStatus(String id, ApplicationStatus status) => _col.doc(id).update({
