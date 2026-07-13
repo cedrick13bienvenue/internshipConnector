cb8fba577c33e0d82fbd415007f492852cfe7165
bienvenue cedrick <cedrick4smith@gmail.com>
2026-07-13 13:02:52 +0200

    Auto-convert Google Drive and Dropbox share links to embed URLs

--- lib/features/applications/presentation/pages/resume_viewer_page.dart
+++ lib/features/applications/presentation/pages/resume_viewer_page.dart
@@ -22,15 +22,29 @@ class _ResumeViewerPageState extends State<ResumeViewerPage> {
   late final String _viewId;
   bool _loaded = false;
 
+  static String _toEmbedUrl(String raw) {
+    // Google Drive: .../file/d/ID/view?... → .../file/d/ID/preview
+    final gdrive = RegExp(r'drive\.google\.com/file/d/([^/]+)');
+    final match = gdrive.firstMatch(raw);
+    if (match != null) {
+      return 'https://drive.google.com/file/d/${match.group(1)}/preview';
+    }
+    // Dropbox: dl=0 → raw=1
+    if (raw.contains('dropbox.com')) {
+      return raw.replaceAll('dl=0', 'raw=1').replaceAll('www.dropbox.com', 'dl.dropboxusercontent.com');
+    }
+    return raw;
+  }
+
   @override
   void initState() {
     super.initState();
     _viewId = 'resume-viewer-${DateTime.now().microsecondsSinceEpoch}';
-    final url = widget.url;
+    final embedUrl = _toEmbedUrl(widget.url);
 
     ui.platformViewRegistry.registerViewFactory(_viewId, (int id) {
       final iframe = html.IFrameElement()
-        ..src = url
+        ..src = embedUrl
         ..style.border = 'none'
         ..style.width = '100%'
         ..style.height = '100%'
